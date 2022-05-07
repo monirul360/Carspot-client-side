06a798b1d510b98889c73d53c49bfa73de5b236e
monirul360 <monirulislam65361@gmail.com>
2022-05-08 03:16:56 +0600

    login page make and style add

---
+++ src/Page/Login/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+const Login = () => {
+    return (
+        <div>
+            <div className="login-container">
+                <h2>Sign In To Your Account</h2>
+                <div className="form-content">
+                    <form >
+                        <label htmlFor="email"><p>Email</p></label>
+                        <input type="email" name="email" placeholder='Enter your email' id="email" />
+                        <label htmlFor="password"><p>Password</p></label>
+                        <input type="password" name="password" placeholder='Enter your password' id="password" />
+                        <input type="submit" value="Login With Us" />
+                    </form><br />
+                    <Link id='register-style' to='/register'>Create new account</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
