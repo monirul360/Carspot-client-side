492a921cde98f881947c382f4eef5f8f88c04930
monirul360 <monirulislam65361@gmail.com>
2022-05-08 02:39:41 +0600

    items page content add and style apply

--- src/Page/Home/Item/Item.js
+++ src/Page/Home/Item/Item.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Item.css';
 const Item = ({ item }) => {
+    const sliceDes = item.description.slice(0, 110);
     return (
         <div>
             <div className="item-container">
                 <div className="show-item">
                     <div className="show-item-img">
                         <img src={item.img} alt="" />
+                        <div className='item-title'>
+                            <span>Item</span>
+                        </div>
+                        <div className="price">
+                            <span>${item.price}</span>
+                        </div>
                     </div>
                     <div className="show-item-text">
-                        <img src="" alt="" />
                         <h3>{item.name}</h3>
-                        <p>{item.description}</p>
-                        <p>{item.supplier}</p>
-                        <p>{item.quantity}</p>
+                        <p>{sliceDes}</p>
+                        <ul>
+                            <li>Supplier: {item.supplier}</li>
+                            <li>Quantity: {item.quantity}</li>
+                        </ul>
+                        <Link to='/' id='item-update'>update</Link>
                     </div>
                 </div>
             </div>
