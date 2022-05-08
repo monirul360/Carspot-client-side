a450dda2497ac59a0be613b5e79fbde8ef217036
monirul360 <monirulislam65361@gmail.com>
2022-05-08 16:15:22 +0600

    private route setup and loading page setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Error from './Page/Error/Error';
 import Home from './Page/Home/Home/Home';
 import Inventory from './Page/Inventory/Inventory';
 import Login from './Page/Login/Login';
+import Private from './Page/Private/Private';
 import Register from './Page/Register/Register';
 import Footer from './Page/Share/Footer/Footer';
 import Header from './Page/Share/Header/Header';
@@ -17,7 +18,11 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/inventory/:id' element={<Inventory></Inventory>}></Route>
+        <Route path='/inventory/:id' element={
+          <Private>
+            <Inventory></Inventory>
+          </Private>
+        }></Route>
         <Route path='*' element={<Error></Error>}></Route>
       </Routes >
       <Footer></Footer>
